8241a09f7d986bcd19f386f69ae2358d010e958b
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-10-26 13:48:51 -0500

    feat(album): add top bar to album detail

--- app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumDetailScreen.kt
+++ app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumDetailScreen.kt
@@ -2,15 +2,33 @@ package com.example.vinylsapp.album.ui.elements
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AlbumDetailScreen() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Regresar")
+                    }
+                }
+            )
+        }
     ) { innerPadding ->
         Surface(modifier = Modifier.padding(innerPadding)) {
             AlbumCover()
