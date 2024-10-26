31ea438e841273c3e3e9b00ea803e2cb2760b642
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-10-26 17:31:14 -0500

    feat: install material extended icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.coil3.coil.compose)
     implementation(libs.coil.network.okhttp)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
