9d56b497c1ce98f55f66469f1eb7e33e28191502
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-10-26 15:27:27 -0500

    feat(track): add repository

--- app/src/main/java/com/example/vinylsapp/album/tracks/repositories/services/NetworkTrackServiceAdapter.kt
+++ app/src/main/java/com/example/vinylsapp/album/tracks/repositories/services/NetworkTrackServiceAdapter.kt
@@ -1,9 +1,10 @@
 package com.example.vinylsapp.album.tracks.repositories.services
 
+import com.example.vinylsapp.album.tracks.models.Track
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface NetworkTrackServiceAdapter {
     @GET("albums/{albumId}/tracks")
-    suspend fun fetchTracks(@Path("albumId") albumId: Int)
+    suspend fun fetchTracks(@Path("albumId") albumId: Int): List<Track>
 }
