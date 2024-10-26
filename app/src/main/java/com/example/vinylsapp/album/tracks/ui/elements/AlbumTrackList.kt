519419ce949c52d815e5ab3d614f0b3b3ee8012d
JhormanGalindo <j.galindop@uniandes.edu.co>
2024-10-26 16:46:50 -0500

    refactor(track): view empty tracks

--- app/src/main/java/com/example/vinylsapp/album/tracks/ui/elements/AlbumTrackList.kt
+++ app/src/main/java/com/example/vinylsapp/album/tracks/ui/elements/AlbumTrackList.kt
@@ -2,6 +2,7 @@ package com.example.vinylsapp.album.tracks.ui.elements
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.vinylsapp.album.tracks.models.Track
 
@@ -21,11 +23,24 @@ fun AlbumTrackList(tracks: List<Track>) {
             style = MaterialTheme.typography.titleMedium,
             modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
         )
-        LazyColumn {
-            items(tracks) {
-                TrackItem(track = it)
-                Box(modifier = Modifier.height(12.dp))
+        if (tracks.isEmpty()) {
+            Text(
+                "No existen canciones asociadas álbum",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp),
+                style = MaterialTheme.typography.titleMedium,
+
+                )
+        } else {
+            LazyColumn {
+                items(tracks) {
+                    TrackItem(track = it)
+                    Box(modifier = Modifier.height(12.dp))
+                }
             }
         }
+
     }
 }
