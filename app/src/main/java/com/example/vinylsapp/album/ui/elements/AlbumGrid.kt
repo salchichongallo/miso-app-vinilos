d9e5dbf67196a04d4b081730291ac10177d6a94f
JhormanGalindo <j.galindop@uniandes.edu.co>
2024-10-26 19:35:49 -0500

    refactor(album): view album list align

--- app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumGrid.kt
+++ app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumGrid.kt
@@ -3,7 +3,6 @@ package com.example.vinylsapp.album.ui.elements
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -12,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.example.vinylsapp.album.models.Album
 
 @Composable
@@ -33,8 +31,7 @@ fun AlbumGrid(albums: List<Album>) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(start = 16.dp),
+            modifier = Modifier.fillMaxSize()
         ) {
             items(albums.size) { index ->
                 AlbumItem(album = albums[index])
--- app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumItem.kt
+++ app/src/main/java/com/example/vinylsapp/album/ui/elements/AlbumItem.kt
@@ -1,5 +1,7 @@
 package com.example.vinylsapp.album.ui.elements
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +12,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -21,22 +25,43 @@ import com.example.vinylsapp.album.models.Album
 
 @Composable
 fun AlbumItem(album: Album) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        AsyncImage(
-            model = album.cover,
-            error = painterResource(R.drawable.default_album_cover),
-            contentDescription = album.name,
-            modifier = Modifier.size(100.dp),
-            contentScale = ContentScale.Crop,
-        )
-        Text(
-            text = album.name, maxLines = 1, overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier
-                .width(100.dp)
-                .padding(top = 4.dp, bottom = 2.dp),
-        )
-        Text(text = album.genre.value, style = MaterialTheme.typography.bodySmall)
-        Box(modifier = Modifier.height(24.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable {
+                println(album.name)
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Column {
+            Box(modifier = Modifier.height(12.dp))
+            AsyncImage(
+                model = album.cover,
+                error = painterResource(R.drawable.default_album_cover),
+                contentDescription = album.name,
+                modifier = Modifier.size(100.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Text(
+                text = album.name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+                    .width(100.dp)
+                    .padding(top = 4.dp, bottom = 2.dp),
+            )
+            Text(
+                text = album.genre.value,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Left
+            )
+            Box(modifier = Modifier.height(12.dp))
+        }
+
     }
 }
