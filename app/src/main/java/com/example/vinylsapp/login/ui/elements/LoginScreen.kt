31c6c7069f8ab9fa625245e8af24015df02d97ff
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-10-26 21:35:11 -0500

    feat(login): add screen hero

--- app/src/main/java/com/example/vinylsapp/login/ui/elements/LoginScreen.kt
+++ app/src/main/java/com/example/vinylsapp/login/ui/elements/LoginScreen.kt
@@ -1,13 +1,16 @@
 package com.example.vinylsapp.login.ui.elements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -16,8 +19,18 @@ import com.example.vinylsapp.models.AppRoutes
 @Composable
 fun LoginScreen(navController: NavController) {
     Scaffold { innerPadding ->
-        Surface(modifier = Modifier.padding(innerPadding)) {
-            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+        Surface(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) {
+                LoginHero()
+                Box(modifier = Modifier.height(24.dp))
                 LoginOption(
                     title = "Ingresar como invitado",
                     description = "Puedes explorar los álbumes y artistas.",
