04ab26ee33cd9019989a8647753012233dd5e883
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-10-31 20:55:26 -0500

    test: init compose tests

--- app/src/androidTest/java/com/example/vinylsapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/vinylsapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,13 @@
 package com.example.vinylsapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +15,13 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.vinylsapp", appContext.packageName)
+    fun guest_option_exists() {
+        composeTestRule.onNodeWithText("Ingresar como invitado").assertIsDisplayed()
     }
 }
