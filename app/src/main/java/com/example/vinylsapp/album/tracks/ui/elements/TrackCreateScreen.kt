1d9e6a9a2211006b0b878b64405ad22fcc034222
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-11-10 11:59:12 -0500

    refactor(track): separate alerts from create screen

--- app/src/main/java/com/example/vinylsapp/album/tracks/ui/elements/TrackCreateScreen.kt
+++ app/src/main/java/com/example/vinylsapp/album/tracks/ui/elements/TrackCreateScreen.kt
@@ -3,22 +3,15 @@ package com.example.vinylsapp.album.tracks.ui.elements
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.rounded.Error
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -66,40 +59,13 @@ fun TrackCreateScreen(viewModel: TrackCreateViewModel, navController: NavControl
                 TrackNewForm(viewModel, navController)
 
                 if (viewModel.isSuccessModalVisible) {
-                    AlertDialog(
-                        onDismissRequest = { viewModel.isSuccessModalVisible = false },
-                        title = { Text("Track asociado") },
-                        text = { Text("El track ha sido asociado al álbum ${viewModel.album.name}") },
-                        confirmButton = {
-                            Button(onClick = { viewModel.isSuccessModalVisible = false }) {
-                                Text("Aceptar")
-                            }
-                        }
-                    )
+                    TrackNewSuccessAlert(
+                        album = viewModel.album.name,
+                        onDismiss = { viewModel.isSuccessModalVisible = false })
                 }
 
                 if (viewModel.isErrorModalVisible) {
-                    AlertDialog(
-                        onDismissRequest = { viewModel.isErrorModalVisible = false },
-                        title = {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text("Ha ocurrido un error")
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Icon(
-                                    imageVector = Icons.Rounded.Error,
-                                    contentDescription = "Error Icon",
-                                    tint = MaterialTheme.colorScheme.error,
-                                    modifier = Modifier.size(24.dp)
-                                )
-                            }
-                        },
-                        text = { Text("Ocurrió un error al agregar el track, intenta nuevamente por favor.") },
-                        confirmButton = {
-                            Button(onClick = { viewModel.isErrorModalVisible = false }) {
-                                Text("Listo")
-                            }
-                        }
-                    )
+                    TrackNewErrorAlert(onDismiss = { viewModel.isErrorModalVisible = false })
                 }
             }
         }
