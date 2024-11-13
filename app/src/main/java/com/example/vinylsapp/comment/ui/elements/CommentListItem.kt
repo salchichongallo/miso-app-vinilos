6df0a651d662d88dcc1eed275e7f2c2b53d75b6e
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-11-12 22:28:04 -0500

    feat(comment): render rating bar in headling

--- app/src/main/java/com/example/vinylsapp/comment/ui/elements/CommentListItem.kt
+++ app/src/main/java/com/example/vinylsapp/comment/ui/elements/CommentListItem.kt
@@ -1,18 +1,24 @@
 package com.example.vinylsapp.comment.ui.elements
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.vinylsapp.comment.models.Comment
 
 @Composable
 fun CommentListItem(comment: Comment) {
     ListItem(
         headlineContent = {
-            Text("Headline goes here")
+            RatingBar(stars = comment.rating)
+            Box(modifier = Modifier.height(8.dp))
         },
         supportingContent = {
             Text(comment.description)
+            Box(modifier = Modifier.height(8.dp))
         },
     )
 }
