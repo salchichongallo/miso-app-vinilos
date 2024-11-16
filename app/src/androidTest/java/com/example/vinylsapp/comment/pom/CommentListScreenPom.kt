9881d39676a3a1992b33f7bc9daeb3f63439c923
leinerbarrios <lj.barrios@uniandes.edu.co>
2024-11-16 16:14:45 -0500

    refactor: 💡 shouldCreateCommentSuccessfully test is improved

--- app/src/androidTest/java/com/example/vinylsapp/comment/pom/CommentListScreenPom.kt
+++ app/src/androidTest/java/com/example/vinylsapp/comment/pom/CommentListScreenPom.kt
@@ -2,7 +2,11 @@ package com.example.vinylsapp.comment.pom
 
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 
 class CommentListScreenPom(val rule: ComposeTestRule) {
-    fun createCommentButton() = rule.onNodeWithTag("CreateCommentButton")
+    fun openCommentCreateModal() {
+        createCommentButton().performClick()
+    }
+    private fun createCommentButton() = rule.onNodeWithTag("CreateCommentButton")
 }
