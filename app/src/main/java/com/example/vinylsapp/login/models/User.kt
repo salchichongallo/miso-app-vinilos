381bab908e93c1a1b3e882e9ab19d09b4c916c45
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-11-16 10:30:52 -0500

    fix(login): fix isCollector comparison

--- app/src/main/java/com/example/vinylsapp/login/models/User.kt
+++ app/src/main/java/com/example/vinylsapp/login/models/User.kt
@@ -1,5 +1,5 @@
 package com.example.vinylsapp.login.models
 
 data class User(val role: Role) {
-    val isCollector: Boolean get() = role == Role.Visitor
+    val isCollector: Boolean get() = role == Role.Collector
 }
