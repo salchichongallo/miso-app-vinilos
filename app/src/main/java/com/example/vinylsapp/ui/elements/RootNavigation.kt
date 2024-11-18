0d5a7a242f32d33cbf7a32c9493b98a0e83b0031
leinerbarrios <lj.barrios@uniandes.edu.co>
2024-11-18 08:33:00 -0500

    feat: 🎸 FloatingActionButton to create album is added
    ✅ Closes: 07

--- app/src/main/java/com/example/vinylsapp/ui/elements/RootNavigation.kt
+++ app/src/main/java/com/example/vinylsapp/ui/elements/RootNavigation.kt
@@ -56,7 +56,7 @@ fun RootNavigation(
             startDestination = AppRoutes.Login.value,
         ) {
             composable(route = AppRoutes.Albums.value) {
-                AlbumListScreen(viewModel = albumListViewModel, navController = navController)
+                AlbumListScreen(viewModel = albumListViewModel, navController = navController, userViewModel = userViewModel)
             }
 
             composable(route = AppRoutes.Artists.value) {
