6db8c094a60ca2a61363b54266e0b0bce4c9f30a
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-11-19 22:11:25 -0500

    feat(artist): add artist album item

--- app/src/main/java/com/example/vinylsapp/artist/ui/elements/ArtistDetailScreen.kt
+++ app/src/main/java/com/example/vinylsapp/artist/ui/elements/ArtistDetailScreen.kt
@@ -1,14 +1,15 @@
 package com.example.vinylsapp.artist.ui.elements
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.vinylsapp.album.models.Album
+import com.example.vinylsapp.album.models.AlbumGenre
 import com.example.vinylsapp.artist.models.Artist
 import com.example.vinylsapp.ui.elements.VinylsBottomAppBar
 
@@ -33,6 +34,17 @@ fun ArtistDetailScreen(navController: NavController, artist: Artist) {
             Column {
                 ArtistDetailHero(artist = artist)
                 ArtistDetailDescription(description = artist.description)
+                Spacer(Modifier.height(8.dp))
+                SectionHeader(title = "Álbumes")
+                ArtistAlbumItem(
+                    album = Album(
+                        id = 1, name = "Foo",
+                        genre = AlbumGenre.FOLK,
+                        releaseDate = "1990",
+                        cover = "https://google.com",
+                    ),
+                    navController = navController
+                )
             }
         }
     }
