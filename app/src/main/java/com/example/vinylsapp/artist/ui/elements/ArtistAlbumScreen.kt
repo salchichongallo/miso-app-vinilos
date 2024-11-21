c40e97468008c6ac93a0eeb750baaf53b6d5e6b9
Alejandro Bogotá <l.bogotab@uniandes.edu.co>
2024-11-20 21:44:27 -0500

    [FEATURE]: Cancel & Create buttons

--- app/src/main/java/com/example/vinylsapp/artist/ui/elements/ArtistAlbumScreen.kt
+++ app/src/main/java/com/example/vinylsapp/artist/ui/elements/ArtistAlbumScreen.kt
@@ -3,15 +3,18 @@ package com.example.vinylsapp.artist.ui.elements
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +22,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -57,6 +61,31 @@ fun ArtistAlbumScreen(navController: NavController, viewModel: ArtistAlbumViewMo
                     modifier = Modifier.size(250.dp)
                 )
                 Text("Agregar ${viewModel.artistName} a álbum")
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    OutlinedButton(
+                        onClick = { navController.popBackStack() },
+                        modifier = Modifier
+                            .weight(1f)
+                            .testTag("CancelArtistAlbumButton")
+                    ) {
+                        Text("Cancelar y volver")
+                    }
+
+                    Button(
+                        onClick = {  },
+                        modifier = Modifier
+                            .weight(1f)
+                            .testTag("CreateTrackButton")
+                    ) {
+                        Text("Agregar")
+                    }
+                }
             }
 
         }
