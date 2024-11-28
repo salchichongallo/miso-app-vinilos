7295a3d8230e15c567af6a340ba79d30b5d95a38
JhormanGalindo <j.galindop@uniandes.edu.co>
2024-11-27 20:07:32 -0500

    test: 💍 E2E: view artist detail

--- app/src/androidTest/java/com/example/vinylsapp/artist/ArtistTest.kt
+++ app/src/androidTest/java/com/example/vinylsapp/artist/ArtistTest.kt
@@ -4,9 +4,12 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.vinylsapp.album.mocks.AlbumRepositoryMock
+import com.example.vinylsapp.album.models.Album
+import com.example.vinylsapp.album.models.AlbumGenre
 import com.example.vinylsapp.album.tracks.mock.TrackRepositoryMock
 import com.example.vinylsapp.artist.mocks.ArtistRepositoryMock
 import com.example.vinylsapp.artist.models.Artist
+import com.example.vinylsapp.artist.pom.ArtistDetailPom
 import com.example.vinylsapp.artist.pom.ArtistScreenPom
 import com.example.vinylsapp.login.pom.LoginPom
 import com.example.vinylsapp.shared.pom.NavigationPom
@@ -21,6 +24,7 @@ import org.junit.runner.RunWith
 class ArtistTest {
     private lateinit var login: LoginPom
     private lateinit var artistList: ArtistScreenPom
+    private lateinit var artistDetail: ArtistDetailPom
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -29,6 +33,7 @@ class ArtistTest {
     fun setUp() {
         login = LoginPom(composeTestRule)
         artistList = ArtistScreenPom(composeTestRule)
+        artistDetail = ArtistDetailPom(composeTestRule)
     }
 
     @After
@@ -81,5 +86,51 @@ class ArtistTest {
 
         val totalArtistExpected = 1
         artistList.assertArtistItemCount(totalArtistExpected)
+    }
+
+    @Test
+    fun validateDetailArtist() {
+        val navigationPom = NavigationPom(composeTestRule)
+        val artistMock = Artist(
+            id = 100,
+            name = "Bruno Mars",
+            image = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
+            description = "Cantante de pop",
+            birthDate = "1984-08-01T00:00:00.000Z",
+            albums = listOf(
+                Album(
+                    id = 101,
+                    name = "Poeta del pueblo 1",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.SALSA,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                ),
+                Album(
+                    id = 102,
+                    name = "Poeta del pueblo 2",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.ROCK,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                )
+            )
+        )
+        val artistListMock = listOf(artistMock)
+        composeTestRule.setContent {
+            RootNavigation(
+                albumRepo = AlbumRepositoryMock(listOf()),
+                trackRepository = TrackRepositoryMock(mutableListOf()),
+                artistRepository = ArtistRepositoryMock(artistListMock)
+            )
+        }
+
+        login.loginAsCollector()
+        navigationPom.navigateToArtistScreen()
+
+        artistList.clickArtistItem(0)
+        artistDetail.isDetailArtistDisplayed(artistMock.name)
+
+        artistDetail.hasNameArtist(artistMock.name)
+
+
     }
 }
