f10bfc9170ca400969936adc21b2cf0543d5e7dc
JhormanGalindo <j.galindop@uniandes.edu.co>
2024-11-27 21:04:17 -0500

    test: 💍 E2E: should select another artist

--- app/src/androidTest/java/com/example/vinylsapp/artist/ArtistTest.kt
+++ app/src/androidTest/java/com/example/vinylsapp/artist/ArtistTest.kt
@@ -1,7 +1,9 @@
 package com.example.vinylsapp.artist
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.vinylsapp.album.mocks.AlbumRepositoryMock
 import com.example.vinylsapp.album.models.Album
@@ -25,6 +27,8 @@ class ArtistTest {
     private lateinit var login: LoginPom
     private lateinit var artistList: ArtistScreenPom
     private lateinit var artistDetail: ArtistDetailPom
+    private lateinit var artistMock1: Artist
+    private lateinit var artistMock2: Artist
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -34,6 +38,52 @@ class ArtistTest {
         login = LoginPom(composeTestRule)
         artistList = ArtistScreenPom(composeTestRule)
         artistDetail = ArtistDetailPom(composeTestRule)
+        artistMock1 = Artist(
+            id = 100,
+            name = "Bruno Mars 1",
+            image = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
+            description = "Cantante de pop 1",
+            birthDate = "1984-08-01T00:00:00.000Z",
+            albums = listOf(
+                Album(
+                    id = 101,
+                    name = "Poeta del pueblo 1",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.SALSA,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                ),
+                Album(
+                    id = 102,
+                    name = "Poeta del pueblo 2",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.ROCK,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                )
+            )
+        )
+        artistMock2 = Artist(
+            id = 101,
+            name = "Bruno Mars 2",
+            image = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
+            description = "Cantante de pop 2",
+            birthDate = "1984-08-01T00:00:00.000Z",
+            albums = listOf(
+                Album(
+                    id = 101,
+                    name = "Poeta del pueblo 1",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.SALSA,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                ),
+                Album(
+                    id = 102,
+                    name = "Poeta del pueblo 2",
+                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
+                    genre = AlbumGenre.ROCK,
+                    releaseDate = "2024-08-01T00:00:00.000Z",
+                )
+            )
+        )
     }
 
     @After
@@ -65,14 +115,7 @@ class ArtistTest {
     @Test
     fun shouldViewOnlyOneArtist() {
         val navigationPom = NavigationPom(composeTestRule)
-        val artistMock = Artist(
-            id = 100,
-            name = "Bruno Mars",
-            image = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            description = "Cantante de pop",
-            birthDate = "1984-08-01T00:00:00.000Z"
-        )
-        val artistListMock = listOf(artistMock)
+        val artistListMock = listOf(artistMock1)
         composeTestRule.setContent {
             RootNavigation(
                 albumRepo = AlbumRepositoryMock(listOf()),
@@ -91,30 +134,7 @@ class ArtistTest {
     @Test
     fun validateDetailArtist() {
         val navigationPom = NavigationPom(composeTestRule)
-        val artistMock = Artist(
-            id = 100,
-            name = "Bruno Mars",
-            image = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            description = "Cantante de pop",
-            birthDate = "1984-08-01T00:00:00.000Z",
-            albums = listOf(
-                Album(
-                    id = 101,
-                    name = "Poeta del pueblo 1",
-                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-                    genre = AlbumGenre.SALSA,
-                    releaseDate = "2024-08-01T00:00:00.000Z",
-                ),
-                Album(
-                    id = 102,
-                    name = "Poeta del pueblo 2",
-                    cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-                    genre = AlbumGenre.ROCK,
-                    releaseDate = "2024-08-01T00:00:00.000Z",
-                )
-            )
-        )
-        val artistListMock = listOf(artistMock)
+        val artistListMock = listOf(artistMock1)
         composeTestRule.setContent {
             RootNavigation(
                 albumRepo = AlbumRepositoryMock(listOf()),
@@ -127,10 +147,35 @@ class ArtistTest {
         navigationPom.navigateToArtistScreen()
 
         artistList.clickArtistItem(0)
-        artistDetail.isDetailArtistDisplayed(artistMock.name)
+        artistDetail.isDetailArtistDisplayed(artistMock1.name)
 
-        artistDetail.hasNameArtist(artistMock.name)
+        artistDetail.hasNameArtist(artistMock1.name)
 
 
     }
+
+    @Test
+    fun shouldSelectAnotherArtist() {
+        val navigationPom = NavigationPom(composeTestRule)
+        val artistListMock = listOf(artistMock1, artistMock2)
+        composeTestRule.setContent {
+            RootNavigation(
+                albumRepo = AlbumRepositoryMock(listOf()),
+                trackRepository = TrackRepositoryMock(mutableListOf()),
+                artistRepository = ArtistRepositoryMock(artistListMock)
+            )
+        }
+
+        login.loginAsCollector()
+        navigationPom.navigateToArtistScreen()
+
+        artistList.clickArtistItem(0)
+        artistDetail.isDetailArtistDisplayed(artistMock1.name)
+
+        composeTestRule.onNodeWithContentDescription("Regresar").performClick()
+
+        artistList.clickArtistItem(1)
+        artistDetail.isDetailArtistDisplayed(artistMock2.name)
+    }
+
 }
